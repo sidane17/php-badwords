58e1b5e8e1deb37cd6b25897866b1df5b7ec8d0c
sidane17 <khalilsidane89@gmail.com>
2024-06-11 18:19:17 +0200

    milstone 1

---
+++ badW.php
@@ -0,0 +1,31 @@
+<?php
+//Per prima cosa, recupero i dati passati nella query string
+//lettura veloce: https://it.wikipedia.org/wiki/Query_string
+//esempio: http://localhost/pagina?chiave=valore&nome=luca&cognome=lambiase
+$nome = $_GET["nome"] ?? "Utente";
+$cognome = $_GET["cognome"] ?? "Anonimo";
+
+//preparo la stringa in anticipo, così l'html resta più leggibile
+$msg = "$nome $cognome";
+$str = strlen($msg);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PHP World</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+<h1><?= $msg; ?></h1>
+<h2>La Lunghezza della stringa è:<?= $str; ?></h1>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+</head>
+<body>
+        <form action="badW.php" method="GET">
+
+        <input type="text" placeholder="Scrivi il tuo nome" name="nome">
+        <input type="text" placeholder="Scrivi il tuo cognome" name="cognome">
+
+        <button type="submit">Invia</button>
+
+        </form>
+    
+</body>
+</html>
